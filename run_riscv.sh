9b45978d15f91b30f792ab1154f35fe01d3cebc7
TomLiu <79390871+Tomlord1122@users.noreply.github.com>
2025-07-08 08:18:40 +0800

    Update run_riscv.sh

--- run_riscv.sh
+++ run_riscv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 TARGET_RUN="/opt/qemu-custom/bin/qemu-riscv64 -L /opt/riscv/sysroot -cpu rv64,v=true,zba=true,zbb=true,zbs=true,vlen=128,vext_spec=v1.0 -d plugin -plugin /home/tomlord/workspace/Speckle/insn.so -D"
-INPUT_TYPE=ref # THIS MUST BE ON LINE 4 for an external sed command to work!
+INPUT_TYPE=ref #this line was auto-generated from gen_binaries.sh
                 # this allows us to externally set the INPUT_TYPE this script will execute
 
-BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 464.h264ref 471.omnetpp 473.astar 453.povray 450.soplex)
-# BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar 433.milc 444.namd 447.dealII 450.soplex 453.povray 470.lbm 482.sphinx3)
+BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar)
+# BENCHMARKS=(401.bzip2)
 
 base_dir=$PWD
 # Find the next available output directory number
@@ -16,39 +16,62 @@ done
 output_dir="${base_dir}/output_${output_num}"
 mkdir -p "${output_dir}"
 
-for b in ${BENCHMARKS[@]}; do
-   echo " -== ${b} ==-"
+# Function to run a single benchmark
+run_benchmark() {
+    local b=$1
+    local base_dir=$2
+    local output_dir=$3
+    
+    echo " -== ${b} ==-"
+    
+    # 為每個 benchmark 創建獨立的子目錄
+    local benchmark_output_dir="${output_dir}/${b}"
+    mkdir -p "${benchmark_output_dir}"
+    
+    cd ${base_dir}/${b}
+    SHORT_EXE=${b##*.}
+    if [ $b == "483.xalancbmk" ]; then 
+        SHORT_EXE=Xalan
+    fi
+    if [ $b == "482.sphinx3" ]; then
+        SHORT_EXE=sphinx_livepretend
+    fi
+    
+    # 讀取命令文件
+    IFS=$'\n' read -d '' -r -a commands < ${base_dir}/commands/${b}.${INPUT_TYPE}.cmd
+    
+    # 創建 benchmark 的總體輸出文件
+    local benchmark_log="${benchmark_output_dir}/${b}_complete.log"
+    echo "Starting benchmark ${b} at $(date)" > "${benchmark_log}"
+    
+    # 執行命令
+    count=0
+    for input in "${commands[@]}"; do
+        if [[ ${input:0:1} != '#' ]]; then
+            # 個別命令的輸出文件
+            local cmd_log="${benchmark_output_dir}/${SHORT_EXE}_${count}.log"
+            cmd="${TARGET_RUN} ${cmd_log} ${SHORT_EXE} ${input}"
+            
+            echo "Executing: $cmd" | tee -a "${benchmark_log}"
+            eval $cmd 2>&1 | tee -a "${benchmark_log}"
+            
+            ((count++))
+        fi
+    done
+    
+    echo "Completed ${b} at $(date)" >> "${benchmark_log}"
+    echo "Completed ${b}"
+}
 
-   cd ${base_dir}/${b}
-   SHORT_EXE=${b##*.} # cut off the numbers ###.short_exe
-   if [ $b == "483.xalancbmk" ]; then 
-      SHORT_EXE=Xalan #WTF SPEC???
-   fi
-   if [ $b == "482.sphinx3" ]; then
-      SHORT_EXE=sphinx_livepretend
-   fi
-   
-   # read the command file
-   IFS=$'\n' read -d '' -r -a commands < ${base_dir}/commands/${b}.${INPUT_TYPE}.cmd
+# Export the function and variables so they can be used by parallel
+export -f run_benchmark
+export TARGET_RUN
+export INPUT_TYPE
+export base_dir
+export output_dir
 
-   # prepare commands for parallel execution
-   parallel_commands=()
-   count=0
-   for input in "${commands[@]}"; do
-      if [[ ${input:0:1} != '#' ]]; then # allow us to comment out lines in the cmd files
-         # Use the new output directory path
-         cmd="${TARGET_RUN} ${output_dir}/${SHORT_EXE}_${count}.log ${SHORT_EXE} ${input}"
-         parallel_commands+=("$cmd")
-         ((count++))
-      fi
-   done
-
-   # execute commands in parallel using GNU Parallel
-   printf "%s\n" "${parallel_commands[@]}" | parallel -j 2
-
-   echo ""
-
-done
+# Run benchmarks in parallel (max 8 threads)
+printf "%s\n" "${BENCHMARKS[@]}" | parallel -j 8 run_benchmark {} "$base_dir" "$output_dir"
 
 echo ""
 echo "Done!"
